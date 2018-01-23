b65a13292ad64e4635ff348f80397f59c477583a
Tung Fam <tung@pixpie.co>
2018-01-23 19:53:12 +0200

    Initial commit

---
+++ G60L3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  G60L3
+//
+//  Created by Tung Fam on 1/23/18.
+//  Copyright © 2018 Tung Fam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
